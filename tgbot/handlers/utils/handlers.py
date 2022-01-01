5f2af8da95b6808a5941884ade222c51ac874218
andrew3203 <kuand3203@gmail.com>
2022-01-01 20:29:40 +0300

    return to begin

--- tgbot/handlers/utils/handlers.py
+++ tgbot/handlers/utils/handlers.py
@@ -193,10 +193,9 @@ def send_selecting_lvl(update: Update, context: CallbackContext):
 
 def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
     """Remove job with given name. Returns whether job was removed."""
-    if context.job_queue:
-        current_jobs = context.job_queue.get_jobs_by_name(name)
-        if not current_jobs:
-            return False
-        for job in current_jobs:
-            job.schedule_removal()
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
     return True
